2e51c624738d4ce511011cf6e89797aabca9cc78
Memy11th <abdelrahmanabdue11@gmail.com>
2024-11-07 22:12:41 +0300

    creating a gridContainer just to be re-useable

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,10 @@
 import type { Metadata } from "next";
 import "./globals.css";
-
-
+import GridContainer from "@/components/GridContainer";
 
 export const metadata: Metadata = {
   title: "Games store center by M11",
-  description: "The biggest gaming center for all gamers for all platforms ",
+  description: "The biggest gaming center for all gamers for all platforms",
 };
 
 export default function RootLayout({
@@ -15,8 +14,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`antialiased`}>
-        {children}
+      <body className="antialiased h-screen">
+        <GridContainer cols={12}>
+          {/* Render your children here */}
+          <div className="col-span-2 bg-yellow-500">hello</div>
+          <div className="col-span-10 bg-red-600">content</div>
+        </GridContainer>
       </body>
     </html>
   );
