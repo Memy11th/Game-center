15154a048eb0dc85b43f35ac1871d9b0c352b662
Memy11th <abdelrahmanabdue11@gmail.com>
2024-11-08 21:23:30 +0300

    Hero Section almost done

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -3,16 +3,31 @@ import Swipper from './Swipper'
 
 const ITEMS = [
     {
-        name:'',
-        src:'',
+        name:'Spiderman',
+        src:'/poster.webp',
+        card:'/spidervideo.mp4'
+    },
+    {
+        name:'Call of Duty',
+        src:'/call-of-duty-black-ops-6-hero-desktop-01-en-21may24.webp',
+        card:'/call-of-duty-black-ops-6-animated-hero-mobile-01-en-22may24.mp4'
+    },
+    {
+        name:'Cyber PUNK',
+        src:'/cyb.webp',
+        card:'/cyberpunk-2077-phantom-liberty-video-hero-01-en-11sep23.mp4'
+    },
+    {
+        name:'Dragon balls',
+        src:'/Dragon-Ball-Sparking-Zero-Hero-desktop-01-03oct24.webp',
         
-    }
+    },
 ]
 export default function HeroSection() {
     return <>
-    <div className='flex justify-center items-center min-h-screen'>
+    <div className='flex justify-center items-start min-h-screen'>
 
-        <Swipper items={ITEMS}  />
+        <Swipper PaginationImgs items={ITEMS}  />
 
     </div>
     </>
--- src/components/Swipper.tsx
+++ src/components/Swipper.tsx
@@ -4,23 +4,37 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import Image from 'next/image';
 
-export default function Swipper({items,PaginationImgs}:{items:{src:string,card:ReactNode,name?:string}[],PaginationImgs?:boolean}) {
+
+
+export default function Swipper({items,PaginationImgs}:{items:{src:string,card:string,name?:string}[],PaginationImgs?:boolean,NameClass?:string}) {
     return <>
+    <div className='h-full w-full '>
+
+    
         <Swiper
-        spaceBetween={50}
+        spaceBetween={10}
         slidesPerView={1}
+        className={`  h-96  `}
         onSlideChange={() => console.log('slide change')}
         onSwiper={(swiper) => console.log(swiper)}
+        loop={true}
         >
-            {items.map((item,index)=> <SwiperSlide key={index}>
-                <Image alt='Pagination-image' src={item?.src} height={40} width={40} loading='lazy'    />
+            <div className='flex items-start justify-start w-full h-full relative '>
+            {items.map((item,index)=> <SwiperSlide key={index}  >
+                {item.card ? <video className='absolute w-full h-full object-cover object-top  rounded-2xl  inset-0 ' autoPlay={true} muted={true} loop={true} src={item.card }>
+
+                </video>:<Image alt='Pagination-image' src={item.src} fill loading='lazy' className=' absolute w-full h-full object-cover object-top  rounded-2xl  inset-0 '    />}
                 </SwiperSlide> )}
-        
-        
+            </div>
+                
 
-
-        
-        ...
         </Swiper>
+        <div className=' flex p-2 gap-4 mx-2'>
+        {PaginationImgs && items.map((item,index)=>  <div className='rounded-xl overflow-hidden max-w-xl w-full h-40  hover:-translate-y-8 relative z-10 duration-300  ' key={index}>
+                <Image alt='Pagination-image' src={item.src} fill loading='lazy' className=' cursor-pointer w-full h-full object-cover object-right-top rounded-2xl  inset-0  '    />
+                </div> )}
+        </div>
+        
+        </div>
     </>
 }
