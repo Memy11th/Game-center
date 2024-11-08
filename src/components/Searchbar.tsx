cc92a7a30bb6e50d0b301eda11f32cbe1604a590
Memy11th <abdelrahmanabdue11@gmail.com>
2024-11-08 16:44:12 +0300

    Header adjustments

--- src/components/Searchbar.tsx
+++ src/components/Searchbar.tsx
@@ -4,9 +4,9 @@ import { FaSearchengin } from 'react-icons/fa'
 export default function Searchbar() {
     return <>
     <div className={`grid col-span-8`}>
-        <div className='flex justify-between w-full p-2 bg-black/15 outline-1 outline-rose-500 my-2 rounded-lg'>
-        <input className=' bg-black/15 w-full outline-0 text-white ' type="text" placeholder='Search...' />
-        {React.cloneElement(<FaSearchengin/>,{className:'h-5 w-5 text-rose-500 '})}
+        <div className='flex justify-between w-full p-2  bg-black/5  outline-1 outline-rose-500 my-2 rounded-lg'>
+        <input className=' bg-black/15 rounded-2xl p-2 font-semibold  w-full outline-0 text-rose-500 ' type="text" placeholder='Search...' />
+        {React.cloneElement(<FaSearchengin/>,{className:'h-5 w-5 text-rose-500 my-auto '})}
         </div>
     </div>
     
