014ad90aeb3c6a8ddec84c55ea2914acf3c438f0
Memy11th <abdelrahmanabdue11@gmail.com>
2024-11-08 23:02:34 +0300

    progressive loading for photo pagination under the slider in hero section

--- src/components/Swipper.tsx
+++ src/components/Swipper.tsx
@@ -1,40 +1,92 @@
 'use client'
-import React, { ReactNode } from 'react'
+import React, { useEffect, useState } from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import Image from 'next/image';
+import { Autoplay } from 'swiper/modules';
+import type { Swiper as SwiperType } from 'swiper';
 
+export default function Swipper({ items, PaginationImgs }: { items: { src: string, card: string, name?:string }[], PaginationImgs?: boolean, NameClass?: string }) {
+    const [swiper, setSwiper] = useState<SwiperType | null>(null);
+    const [progress, setProgress] = useState(0);
 
+    useEffect(() => {
+        // creating increamental behaviour 
+        const t = setInterval(() => {
+            setProgress((prev) => prev >= 100 ? 100 : prev + 3.5);
+        }, 101);
 
-export default function Swipper({items,PaginationImgs}:{items:{src:string,card:string,name?:string}[],PaginationImgs?:boolean,NameClass?:string}) {
-    return <>
-    <div className='h-full w-full '>
+        return () => clearInterval(t); // clear the t interval to avoid memory leakage when the component un-mounts
+    }, [progress]); 
 
-    
-        <Swiper
-        spaceBetween={10}
-        slidesPerView={1}
-        className={`  h-96  `}
-        onSlideChange={() => console.log('slide change')}
-        onSwiper={(swiper) => console.log(swiper)}
-        loop={true}
-        >
-            <div className='flex items-start justify-start w-full h-full relative '>
-            {items.map((item,index)=> <SwiperSlide key={index}  >
-                {item.card ? <video className='absolute w-full h-full object-cover object-top  rounded-2xl  inset-0 ' autoPlay={true} muted={true} loop={true} src={item.card }>
-
-                </video>:<Image alt='Pagination-image' src={item.src} fill loading='lazy' className=' absolute w-full h-full object-cover object-top  rounded-2xl  inset-0 '    />}
-                </SwiperSlide> )}
-            </div>
-                
-
-        </Swiper>
-        <div className=' flex p-2 gap-4 mx-2'>
-        {PaginationImgs && items.map((item,index)=>  <div className='rounded-xl overflow-hidden max-w-xl w-full h-40  hover:-translate-y-8 relative z-10 duration-300  ' key={index}>
-                <Image alt='Pagination-image' src={item.src} fill loading='lazy' className=' cursor-pointer w-full h-full object-cover object-right-top rounded-2xl  inset-0  '    />
-                </div> )}
-        </div>
+    useEffect(() => {
+        // Reset progress to 0 when slide changes
+            swiper?.on('slideChange', () => setProgress(0));
         
+    }, [swiper]);
+
+    return (
+        <div className='h-full w-full'>
+            <Swiper
+                modules={[Autoplay]}
+                autoplay={{ delay: 3000 }}
+                speed={1000}
+                spaceBetween={10}
+                slidesPerView={1}
+                className='h-96 rounded-2xl'
+                onSlideChange={() => console.log('slide change')}
+                onSwiper={(swiperInstance) => setSwiper(swiperInstance)} // Set swiper instance to state
+                loop={true}
+            >
+                <div className='flex items-start justify-start w-full h-full relative'>
+                    {items.map((item, index) => (
+                        <SwiperSlide key={index}>
+                            {item.card ? (
+                                <video
+                                    className='absolute w-full h-full object-cover object-top rounded-2xl inset-0'
+                                    autoPlay
+                                    muted
+                                    loop
+                                    src={item.card}
+                                />
+                            ) : (
+                                <Image
+                                    alt='Pagination-image'
+                                    src={item.src}
+                                    fill
+                                    loading='lazy'
+                                    className='absolute w-full h-full object-cover object-top rounded-2xl inset-0'
+                                />
+                            )}
+                        </SwiperSlide>
+                    ))}
+                </div>
+            </Swiper>
+
+            {PaginationImgs && (
+                <div className='flex p-2 gap-4 mx-2'>
+                    {items.map((item, index) => (
+                        
+                        <div
+                        className='rounded-xl overflow-hidden max-w-xl w-full h-40 hover:-translate-y-8  relative z-10 duration-300'
+                        key={index}
+                        >
+                            {swiper?.realIndex === index ? <div style={{ width: `${progress}%` }} className='absolute top-0 left-0 h-full duration-300 transition-all bg-black/35 z-20' > 
+                            </div> : null}
+                        
+
+                        <Image
+                            alt='Pagination-image'
+                            src={item.src}
+                            fill
+                            loading='lazy'
+                            className='cursor-pointer w-full h-full object-cover object-right-top rounded-2xl inset-0'
+                        />
+                        
+                    </div>
+                    ))}
+                </div>
+            )}
         </div>
-    </>
+    );
 }
