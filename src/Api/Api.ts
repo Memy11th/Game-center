5e460d71ed8c62dc71fc934ba30557cb7371ebdb
Memy11th <abdelrahmanabdue11@gmail.com>
2024-11-09 21:00:04 +0300

    creating a dynamic fetch function to be reusable

--- src/Api/Api.ts
+++ src/Api/Api.ts
@@ -6,7 +6,12 @@ export const searchGameFn = async function (
     query?:string,
     page:number,
     page_size:number = 20,
+    cache?:number,
     filters?:{filterName:string,option:string}[]
 ){
+    const data = await fetchFn(`${API_URL}games?query=${query}&page_size=${page_size}&page=${page}&${filters?.map((filter)=>`${filter.filterName}=${filter.option}&`).join('')}&key=${API_KEY}`,
+    cache );
+    console.log(data)
+    return data;
     
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
+import { searchGameFn } from "@/Api/Api";
 import HeroSection from "@/components/HeroSection";
-
 export default function Home() {
+  searchGameFn('',1,2,600);
   return <>
   <HeroSection/>
   </>
