d70d0375b8b68805568d5ba0d9bad97959c5454b
Memy11th <abdelrahmanabdue11@gmail.com>
2024-11-12 20:38:02 +0300

    testing out

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,16 @@
 import HeroSection from "@/components/HeroSection";
+import PlatformSection from "@/components/PlatformSection";
 
-
+const platformOptions =[
+  'PC',
+  'Playstation',
+  'XBox'
+]
 export default  function Home() {
   return <>
   <div className="min-h-screen">
   <HeroSection/>
+  <PlatformSection option ={platformOptions[0]}/>
 
   </div>
 
--- src/components/PlatformSection.tsx
+++ src/components/PlatformSection.tsx
@@ -4,8 +4,19 @@ import Image from 'next/image';
 
 
 export default async function PlatformSection({option}:{option:string}) {
-    const {results,count,next,previous} = await searchGameFn('',1,20,3600,[{filterName:'platform',option:option}])
+    const {results,count,next,previous} = await searchGameFn('',1,1,3600,[{filterName:'platform',option:option}])
+    console.log(results)
     return <>
+
+    <div className='w-full grid-cols-12 grid min-h-screen'>
+        <div className='relative col-span-3 h-64  group rounded-lg '>
+
+            <Image fill alt={results[0].name} src={results[0].background_image} className='rounded-lg  group-hover:rotate-3  group-hover:scale-125 duration-300  ' />
+
+        </div>
+        <p>{results[0].name}</p>
+
+    </div>
     
     </>
 }
