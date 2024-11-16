7de08a149563bae7ed40bf33dbe65b3c72890829
Memy11th <abdelrahmanabdue11@gmail.com>
2024-11-16 18:57:34 +0300

    Progress bar enhancments

--- src/components/ProgressBar.tsx
+++ src/components/ProgressBar.tsx
@@ -1,43 +1,27 @@
 'use client'
-
 import { useState, useEffect } from "react";
+import throttle from "lodash.throttle";
 
 const ProgressBar = () => {
   const [scrollPercentage, setScrollPercentage] = useState(0);
 
   useEffect(() => {
-    const handleScroll = () => {
+    const handleScroll = throttle(() => {
       const scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
       const scrollHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-      const progress = (scrollTop / scrollHeight) * 100;
+      const progress = Math.round((scrollTop / scrollHeight) * 100);
       setScrollPercentage(progress);
-    };
+    }, 50); 
 
     window.addEventListener("scroll", handleScroll);
-    return () => window.removeEventListener("scroll", handleScroll); // Cleanup on component unmount
+    return () => window.removeEventListener("scroll", handleScroll);
   }, []);
 
   return (
-    <div style={styles.container}>
-      <div className=" bg-rose-500" style={{ ...styles.bar, width: `${scrollPercentage}%` }} />
+    <div className=" transition-all duration-300 bg-white dark:bg-black" style={{ position: "fixed", top: 0, left: 0, width: "100%", height: "5px", zIndex: 1000 }}>
+      <div className="bg-rose-500 transition-all duration-300" style={{ height: "100%", width: `${scrollPercentage}%`, transition: "width 0.25s ease" }} />
     </div>
   );
-};
-
-const styles = {
-  container: {
-    position: "fixed",
-    top: 0,
-    left: 0,
-    width: "100%",
-    height: "5px",
-    backgroundColor: "#f3f3f3",
-    zIndex: 1000,
-  },
-  bar: {
-    height: "100%",
-    transition: "width 0.25s ease",
-  },
 };
 
 export default ProgressBar;
