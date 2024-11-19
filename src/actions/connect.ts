5ab8da8398b864655623070dc29c93dc752acde9
Memy11th <abdelrahmanabdue11@gmail.com>
2024-11-19 22:05:05 +0300

    HOPE IT WORKS

--- src/actions/connect.ts
+++ src/actions/connect.ts
@@ -1,12 +1,16 @@
-'use server'
 import mongoose from "mongoose";
 let cached = (global as any).mongoose || { conn: null, promise: null };
-const connect = async () => {
+ const connect = async () => {
     if (cached.conn) return cached.conn;
   
     if (!cached.promise) {
       cached.promise = mongoose
-        .connect(process.env.MONGO_URI!, { dbName: "Memy11th", bufferCommands: false })
+        .connect(process.env.MONGO_URI!, {
+          dbName: "Memy11th",
+          bufferCommands: false,
+          serverSelectionTimeoutMS: 5000, // Timeout for finding a server
+          socketTimeoutMS: 45000, // Timeout for establishing a socket
+        })
         .then((mongooseInstance) => {
           console.log("DB connected successfully!");
           return mongooseInstance;
@@ -21,5 +25,6 @@ const connect = async () => {
     cached.conn = await cached.promise;
     return cached.conn;
   };
+
+  export default connect;
   
-export default connect;
